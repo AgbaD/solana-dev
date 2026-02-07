fb1bc8afd8c0b3306bee06eb109f361ee5d8cfe2
Damilare Agba <damilareagba1@gmail.com>
2026-02-07 22:26:12 +0100

    feat: add spl token transfer and metadata scripts

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,5 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../turbin3-wallet.json"
+import wallet from "./wallet/turbin3-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,7 +10,7 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("F7FQpLCoGpb7BXXqutB1t8YdPzNpTmAx3QfBAykFCycT");
 
 // Recipient address
 const to = new PublicKey("<receiver address>");
@@ -18,10 +18,33 @@ const to = new PublicKey("<receiver address>");
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromAta = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey,
+        );
+        console.log(`Your ATA: ${fromAta.address.toBase58()}`);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const toAta = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            to,
+        );
+        console.log(`Recipient ATA: ${toAta.address.toBase58()}`);
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const tx = await transfer(
+            connection,
+            keypair,
+            fromAta.address,
+            toAta.address,
+            keypair,
+            1_000_000n,
+        );
+        console.log(`Transfer txid: ${tx}`);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
